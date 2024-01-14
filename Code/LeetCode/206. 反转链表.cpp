36e1a2babb0f83d02eb3d966e23dc3d818424466
lyx9823 <49195970+lyx9823@users.noreply.github.com>
2024-01-14 23:14:55 +0800

    Update 206. 反转链表.cpp

--- Code/LeetCode/206. 反转链表.cpp
+++ Code/LeetCode/206. 反转链表.cpp
@@ -28,3 +28,19 @@ public:
         return pre;
     }
 };
+
+// offitial solution: https://leetcode.cn/problems/reverse-linked-list/solutions/2361282/206-fan-zhuan-lian-biao-shuang-zhi-zhen-r1jel/
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        return recur(head, nullptr);           // 调用递归并返回
+    }
+private:
+    ListNode* recur(ListNode* cur, ListNode* pre) {
+        if (cur == nullptr) return pre;        // 终止条件
+        ListNode* res = recur(cur->next, cur); // 递归后继节点
+        cur->next = pre;                       // 修改节点引用指向
+        return res;                            // 返回反转链表的头节点
+    }
+};
+
