07ce97117250c5110370c33ce56897c6bc23fe8d
lyx9823 <49195970+lyx9823@users.noreply.github.com>
2024-02-12 21:00:08 +0800

    Create 1. 两数之和.cpp

---
+++ Code/LeetCode/1. 两数之和.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> sorted_nums = nums;
+        sort(sorted_nums.begin(), sorted_nums.end());
+        int len = sorted_nums.size();
+        int left = 0, right = len-1;
+        vector<int> res;
+        int res0, res1;
+        while(left != right) {
+            int sum = sorted_nums[left] + sorted_nums[right];
+            if(sum == target) {
+                res0 = sorted_nums[left];
+                res1 = sorted_nums[right];
+                break;
+            } else if(sum > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        for(int i=0, index=2; i<nums.size(), index>0; i++) {
+            if((res0==nums[i]) || (res1==nums[i])) {
+                res.push_back(i);
+                index--;
+            }
+        }
+        return res;
+    }
+};
