af9b38568c673a111d6330814b6e2c2fffef2139
lyx9823 <49195970+lyx9823@users.noreply.github.com>
2024-02-14 02:13:43 +0800

    Create 151. 反转字符串中的单词.cpp

---
+++ Code/LeetCode/151. 反转字符串中的单词.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string res = "";
+        int left = s.length()-1, right = s.length()-1;
+        while(left>=0) {
+            // 跳过字符串尾部空格
+            while((left>=0) && (s[left] == ' ')) {
+                left--;
+            }
+            // 找到单词尾
+            right = left+1;
+            
+            // 找到单词头
+            while((left>=0) && (s[left]!=' ')) {
+                left--;
+            }
+
+            // 跳过字符串头部空格
+            if(right>0) {
+                res.append(s.substr(left+1, right-left-1)+" ");
+                right = left;
+            }   
+        }
+
+        // 去除最后一个空格
+        return res.substr(0, res.length()-1);
+    }
+};
