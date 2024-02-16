b280a00a3deed9824326f5c7571677101ebd866c
lyx9823 <49195970+lyx9823@users.noreply.github.com>
2024-02-17 03:01:58 +0800

    Update 1047. 删除字符串中的所有相邻重复项.cpp

--- Code/LeetCode/1047. 删除字符串中的所有相邻重复项.cpp
+++ Code/LeetCode/1047. 删除字符串中的所有相邻重复项.cpp
@@ -1,3 +1,4 @@
+// my solution
 class Solution {
 public:
     string removeDuplicates(string s) {
@@ -21,3 +22,21 @@ public:
         return ss;
     }
 };
+
+// offitial solution: https://leetcode.cn/problems/remove-all-adjacent-duplicates-in-string/solutions/643955/shan-chu-zi-fu-chuan-zhong-de-suo-you-xi-4ohr/
+// 在C++中，由于 std::string\texttt{std::string}std::string 类本身就提供了类似「入栈」和「出栈」的接口，因此我们直接将需要被返回的字符串作为栈即可。
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string stk;
+        for (char ch : s) {
+            if (!stk.empty() && stk.back() == ch) {
+                stk.pop_back();
+            } else {
+                stk.push_back(ch);
+            }
+        }
+        return stk;
+    }
+};
+
