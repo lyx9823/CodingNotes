b7ee0a4bc11df46df36bbb8e7d14278c747be0a8
lyx9823 <49195970+lyx9823@users.noreply.github.com>
2024-03-01 01:48:34 +0800

    Update 144. 二叉树的前序遍历.cpp

--- Code/LeetCode/144. 二叉树的前序遍历.cpp
+++ Code/LeetCode/144. 二叉树的前序遍历.cpp
@@ -9,7 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// 递归
+// 递归: https://programmercarl.com/%E4%BA%8C%E5%8F%89%E6%A0%91%E7%9A%84%E9%80%92%E5%BD%92%E9%81%8D%E5%8E%86.html
 class Solution {
 public:
     void recursionTree(TreeNode* node, vector<int>& tree) {
@@ -28,7 +28,7 @@ public:
     }
 };
 
-// 迭代
+// 迭代: https://programmercarl.com/%E4%BA%8C%E5%8F%89%E6%A0%91%E7%9A%84%E8%BF%AD%E4%BB%A3%E9%81%8D%E5%8E%86.html#%E7%AE%97%E6%B3%95%E5%85%AC%E5%BC%80%E8%AF%BE
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
@@ -42,6 +42,32 @@ public:
             result.push_back(node->val);
             if (node->right) st.push(node->right);           // 右（空节点不入栈）
             if (node->left) st.push(node->left);             // 左（空节点不入栈）
+        }
+        return result;
+    }
+};
+
+// 统一迭代法: https://programmercarl.com/%E4%BA%8C%E5%8F%89%E6%A0%91%E7%9A%84%E7%BB%9F%E4%B8%80%E8%BF%AD%E4%BB%A3%E6%B3%95.html#%E6%80%9D%E8%B7%AF
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
+        stack<TreeNode*> st;
+        if (root != NULL) st.push(root);
+        while (!st.empty()) {
+            TreeNode* node = st.top();
+            if (node != NULL) {
+                st.pop();
+                if (node->right) st.push(node->right);  // 右
+                if (node->left) st.push(node->left);    // 左
+                st.push(node);                          // 中
+                st.push(NULL);
+            } else {
+                st.pop();
+                node = st.top();
+                st.pop();
+                result.push_back(node->val);
+            }
         }
         return result;
     }
