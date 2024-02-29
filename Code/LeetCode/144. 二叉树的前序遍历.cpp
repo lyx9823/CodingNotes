e44028f63ead3d32f52478c6cd5f30517b223d1b
lyx9823 <49195970+lyx9823@users.noreply.github.com>
2024-03-01 01:30:58 +0800

    Update 144. 二叉树的前序遍历.cpp

--- Code/LeetCode/144. 二叉树的前序遍历.cpp
+++ Code/LeetCode/144. 二叉树的前序遍历.cpp
@@ -9,6 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// 递归
 class Solution {
 public:
     void recursionTree(TreeNode* node, vector<int>& tree) {
@@ -24,5 +25,24 @@ public:
         recursionTree(root, res);
 
         return res;
+    }
+};
+
+// 迭代
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st;
+        vector<int> result;
+        if (root == NULL) return result;
+        st.push(root);
+        while (!st.empty()) {
+            TreeNode* node = st.top();                       // 中
+            st.pop();
+            result.push_back(node->val);
+            if (node->right) st.push(node->right);           // 右（空节点不入栈）
+            if (node->left) st.push(node->left);             // 左（空节点不入栈）
+        }
+        return result;
     }
 };
