3f327398430e8c935a2e55a9d3cc001b26c8b292
lyx9823 <49195970+lyx9823@users.noreply.github.com>
2024-03-17 23:10:28 +0800

    Create 236. 二叉树的最近公共祖先.cpp

---
+++ Code/LeetCode/236. 二叉树的最近公共祖先.cpp
@@ -0,0 +1,33 @@
+// 思路：回溯，后序遍历 左右中
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if((root == NULL) || (root == p) || (root == q)) {
+            return root;
+        }
+
+        TreeNode *left = lowestCommonAncestor(root->left, p, q);
+        TreeNode *right = lowestCommonAncestor(root->right, p, q);
+
+        if((left != NULL) && (right != NULL)) {
+            return root;
+        }else if((left == NULL) && (right == NULL)) {
+            return NULL;
+        }else if((left == NULL) && (right != NULL)) {
+            return right;
+        }else if((left != NULL) && (right == NULL)) {
+            return left;
+        }
+        return NULL;
+    }
+};
